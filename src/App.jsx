d72f37e7673b48bad6432a8a85ee299066fd0967
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-22 01:17:28 +0530

    fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -33,13 +33,13 @@ function App() {
             }
         }
 
-        if((arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="o") || (arr[3]===arr[4] && arr[4]===arr[5] &&arr[5]==="o") || (arr[7]===arr[6] && arr[7]===arr[8] &&arr[8]==="o") || (arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="o") || (arr[0]===arr[4] && arr[4]===arr[8] &&arr[1]==="o") || (arr[2]===arr[4] && arr[4]===arr[6] &&arr[1]==="o") || (arr[0]===arr[3] && arr[3]===arr[6] &&arr[1]==="o") || (arr[1]===arr[4] && arr[7]===arr[2] &&arr[1]==="o") || (arr[2]===arr[5] && arr[5]===arr[8] &&arr[1]==="o")){
+        if((arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="o") || (arr[3]===arr[4] && arr[4]===arr[5] &&arr[5]==="o") || (arr[7]===arr[6] && arr[7]===arr[8] &&arr[8]==="o") || (arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="o") || (arr[0]===arr[4] && arr[4]===arr[8] &&arr[4]==="o") || (arr[2]===arr[4] && arr[4]===arr[6] &&arr[4]==="o") || (arr[0]===arr[3] && arr[3]===arr[6] &&arr[3]==="o") || (arr[1]===arr[4] && arr[7]===arr[2] &&arr[2]==="o") || (arr[2]===arr[5] && arr[5]===arr[8] &&arr[8]==="o")){
             setWinner("Player1");
             setArr(["","","","","","","","",""]);
         }
 
 
-        if((arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="x") || (arr[3]===arr[4] && arr[4]===arr[5] &&arr[5]==="x") || (arr[7]===arr[6] && arr[7]===arr[8] &&arr[8]==="x") || (arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="x") || (arr[0]===arr[4] && arr[4]===arr[8] &&arr[1]==="x") || (arr[2]===arr[4] && arr[4]===arr[6] &&arr[1]==="x") || (arr[0]===arr[3] && arr[3]===arr[6] &&arr[1]==="x") || (arr[1]===arr[4] && arr[7]===arr[2] &&arr[1]==="x") || (arr[2]===arr[5] && arr[5]===arr[8] &&arr[1]==="x")){
+        if((arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="x") || (arr[3]===arr[4] && arr[4]===arr[5] &&arr[5]==="x") || (arr[7]===arr[6] && arr[7]===arr[8] &&arr[8]==="x") || (arr[0]===arr[1] && arr[1]===arr[2] &&arr[1]==="x") || (arr[0]===arr[4] && arr[4]===arr[8] &&arr[8]==="x") || (arr[2]===arr[4] && arr[4]===arr[6] &&arr[6]==="x") || (arr[0]===arr[3] && arr[3]===arr[6] &&arr[1]==="6") || (arr[1]===arr[4] && arr[7]===arr[2] &&arr[2]==="x") || (arr[2]===arr[5] && arr[5]===arr[8] &&arr[8]==="x")){
             setWinner("Player2");
             setArr(["","","","","","","","",""]);
         }
